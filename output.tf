7ff2b5a8c7aa6d8cb4bad09de1a16a1e9bc3f64c
Richard Duncan <richard@sciomedes.com>
2019-06-16 17:50:46 -0400

    Add storage bucket ARN output

--- output.tf
+++ output.tf
@@ -7,3 +7,8 @@ output "iam_role_arn" {
   description = "ARN for IAM role assumed by lambda function"
   value = "${module.iam-role-lambda.iam_role_arn}"
 }
+
+output "storage_bucket_arn" {
+  description = "The ARN of the S3 storage bucket"
+  value = "${module.s3-storage-bucket.storage_bucket_arn}"
+}
