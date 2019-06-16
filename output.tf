f2ac9e98d37e10a174690c0df43f5fad1837ec19
Richard Duncan <richard@sciomedes.com>
2019-06-16 14:33:04 -0400

    Add output variable
    The role ARN is output by the role creation module and is passed to
    the root module.

--- output.tf
+++ output.tf
@@ -2,3 +2,8 @@ output "logging_bucket_arn" {
   description = "The ARN of the S3 logging bucket"
   value = "${module.s3-logging-bucket.logging_bucket_arn}"
 }
+
+output "iam_role_arn" {
+  description = "ARN for IAM role assumed by lambda function"
+  value = "${module.iam-role-lambda.iam_role_arn}"
+}
