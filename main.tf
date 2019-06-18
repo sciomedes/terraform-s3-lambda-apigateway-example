67cf6eb5a881dfba0e785c4132e5f5f3bc9750b7
Richard Duncan <richard@sciomedes.com>
2019-06-17 20:05:19 -0400

    Improve force_destroy variables in root module

--- main.tf
+++ main.tf
@@ -1,9 +1,6 @@
 locals {
   region = "us-east-2"
 
-  // should non-empty buckets be removed upon 'terraform destroy'
-  force_destroy = true
-  
   # logging bucket variables
   logging_bucket_name = "sciomedes-logging-bucket-1ecf263bd0dd5836"
   logging_lifecycle_12 = 30   // 1 month
@@ -32,6 +29,11 @@ locals {
     Function       = "data-storage"
     Region         = "${local.region}"
   }
+
+  // should non-empty buckets be removed upon 'terraform destroy'
+  // this is useful during development and testing
+  logging_force_destroy = true
+  storage_force_destroy = true
 
   # iam role variables
   role_name = "lambda_role_name"
@@ -68,6 +70,7 @@ module "s3-logging-bucket" {
   #------------------------------------------------------------------------
   region              = "${local.region}"
   logging_bucket_name = "${local.logging_bucket_name}"
+  force_destroy       = "${local.logging_force_destroy}"
 
   #------------------------------------------------------------------------
   # tag resources:
@@ -89,6 +92,7 @@ module "s3-logging-bucket" {
   # expire after this many days:
   # this number MUST be greater than lifecycle_23
   lifecycle_3X = "${local.logging_lifecycle_3X}"
+
 
 }
 
@@ -133,7 +137,7 @@ module "s3-storage-bucket" {
   #------------------------------------------------------------------------
   region              = "${local.region}"
   bucket_name         = "${local.storage_bucket_name}"
-  force_destroy       = "${local.force_destroy}"
+  force_destroy       = "${local.storage_force_destroy}"
 
   #------------------------------------------------------------------------
   # tag resource:
